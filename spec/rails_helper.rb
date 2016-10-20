961d28bda2b207233bb3460579e867de4c58e36c
Juliane Lima <jujulisan@gmail.com>
2016-10-20 14:30:01 -0200

    Remove Warden from rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,9 +6,6 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 
-include Warden::Test::Helpers
-Warden.test_mode!
-
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
