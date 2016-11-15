c4c4a4fdabd9dfc87c68d4e03d91842337139120
Juliane Lima <jujulisan@gmail.com>
2016-11-15 15:05:31 -0200

    Implemented index view with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(email: "test2@test.com", password: "123456", password_confirmation: "123456", first_name: "test", last_name: "test")
+
+puts "User created"
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale")
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been create"
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe "Creation" do
+     before do
+      @user = User.create(email: "test2@test.com", password: "123456", password_confirmation: "123456", first_name: "test", last_name: "test")
+      login_as(@user, scope: :user)
+    end
   	before do
-  		@post = Post.create(date: Date.today, rationale: "Anything")
+  		@post = Post.create(date: Date.today, rationale: "Anything", user_id: @user.id)
   	end
 
   	it "Can be created" do
