3b2cc6de461bdfd5e0ec3dfe495f2280a9729eeb
Juliane Lima <jujulisan@gmail.com>
2017-01-03 16:39:55 -0200

    Added status field in administrate dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-@user = User.create(email: "test2@test.com", password: "123456", password_confirmation: "123456", first_name: "test", last_name: "test")
-
+@user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "test", last_name: "test")
 puts "User created"
+
+AdminUser.create(email: "admin@test.com", password: "123456", password_confirmation: "123456", first_name: "admin", last_name: "admin")
+puts "AdminUser created"
+
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
-
 puts "100 Posts have been create"
